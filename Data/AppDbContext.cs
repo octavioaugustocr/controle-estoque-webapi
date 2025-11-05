464e3ad9c12f2543e9daf0483ff7515a63c12666
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2025-11-04 21:24:43 -0300

    Configurando a propriedade 'Preço' da classe ProdutosModel

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -8,5 +8,10 @@ namespace controle_produtos_webapi.Data
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<ProdutosModel> Produtos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProdutosModel>(entity => entity.Property(p => p.Preco).HasColumnType("decimal(18,2)"));
+        }
     }
 }
