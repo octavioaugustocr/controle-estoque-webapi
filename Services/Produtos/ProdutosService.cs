f1340cac22a00c9e0b7104ca1b68a0cbe1a6b349
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2025-11-04 21:33:15 -0300

    Fazendo a injenção de dependências no ProdutosService

--- Services/Produtos/ProdutosService.cs
+++ Services/Produtos/ProdutosService.cs
@@ -1,6 +1,14 @@
-﻿namespace controle_produtos_webapi.Services.Produtos
+﻿using controle_produtos_webapi.Data;
+
+namespace controle_produtos_webapi.Services.Produtos
 {
     public class ProdutosService : IProdutosService
     {
+        private readonly AppDbContext _appDbContext;
+
+        public ProdutosService(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
     }
 }
