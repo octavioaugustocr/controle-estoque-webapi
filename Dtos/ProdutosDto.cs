81e98966f36c13ac26eed8855acfcba4b40ee188
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2025-11-04 21:55:14 -0300

    Criando a classe ProdutosDto

---
+++ Dtos/ProdutosDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace controle_produtos_webapi.Dtos
+{
+    public class ProdutosDto
+    {
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public string Marca { get; set; }
+        public string Fabricante { get; set; }
+        public string Sabor { get; set; }
+        public DateOnly DataVencimento { get; set; }
+        public int Estoque { get; set; }
+    }
+}
