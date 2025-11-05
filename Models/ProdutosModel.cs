59c0c156d388c650d605a8513ccdd981b11bebbd
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2025-11-04 21:18:13 -0300

    Criando a classe ProdutosModel

---
+++ Models/ProdutosModel.cs
@@ -0,0 +1,14 @@
+﻿namespace controle_produtos_webapi.Models
+{
+    public class ProdutosModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public string Marca { get; set; }
+        public string Fabricante { get; set; }
+        public string Sabor { get; set; }
+        public DateOnly DataVencimento {  get; set; }
+        public int Estoque { get; set; }
+    }
+}
