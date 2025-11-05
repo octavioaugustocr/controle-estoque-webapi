2b2d7902f08d395574470afcd5508df67ad604a6
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2025-11-04 21:22:04 -0300

    Criando a classe AppDbContext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using controle_produtos_webapi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace controle_produtos_webapi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<ProdutosModel> Produtos { get; set; }
+    }
+}
